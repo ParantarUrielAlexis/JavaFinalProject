739a7db06454b728fb49fe7e4bf0ea29eda8ff20
Uriel <leirualexisparantar@gmail.com>
2023-12-06 14:50:41 +0800

    das

--- FinalProject/src/Main.java
+++ FinalProject/src/Main.java
@@ -2,5 +2,8 @@ public class Main
 {
     public static void main(String[] args) {
         new Frame();
+        System.out.print("Kent Gwapo");
+        System.out.print(" Atay push and commit oi!!!!");
     }
+
 }
