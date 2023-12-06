fec758050857322689e9a371f6d88719296f4810
Lance Antor <lanceantor335@gmail.com>
2023-12-06 15:38:25 +0800

    ..

--- FinalProject/src/startPage.java
+++ FinalProject/src/startPage.java
@@ -1,4 +1,30 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class startPage extends JFrame {
+
+
+    private JPanel startPagePanel;
+    private JButton startBtn;
+    private JLabel line1;
+
+    public startPage(){
+        startBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                choosePage choose = new choosePage();
+                setContentPane(choose.choosePagePanel);
+                revalidate();
+            }
+        });
+    }
+    public static void main(String[] args) {
+        startPage app = new startPage();
+        app.setContentPane(app.startPagePanel);
+        app.setSize(1000, 700);
+        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setTitle("Typing Game");
+    }
 }
