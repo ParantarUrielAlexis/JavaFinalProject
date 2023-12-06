2f9778b9ca4d2201d50540de874d29fa118676c5
Uriel <leirualexisparantar@gmail.com>
2023-12-06 16:15:45 +0800

    update

--- FinalProject/src/Main.java
+++ FinalProject/src/Main.java
@@ -2,10 +2,6 @@ public class Main
 {
     public static void main(String[] args) {
         new Frame();
-        System.out.println("Kent Gwapo");
-        System.out.println(" Atay push and commit oi!!!!");
-        System.out.println("  push and commit suicide =) ");
-        System.out.println("  accepted nako???");
     }
 
 }
